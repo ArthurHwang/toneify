3403350a6d20258f462fba51bc46a92eccf3d926
ArthurHwang <ahwang55@gmail.com>
2018-07-23 13:18:18 -0700

    When request to GET route is made, it should respond with coreect JSON body #1

--- server/index.js
+++ server/index.js
@@ -1,7 +1,27 @@
+require('dotenv/config')
 const express = require('express')
+const bodyParser = require('body-parser')
+const { MongoClient} = require('mongodb')
 const app = express()
 
+MongoClient
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
+  .then(client => {
+    const db = client.db('toneify')
+    const collection = db.collection('pedalboards')
 
-app.get('/pedals', (req, res) => {
-  
-})
+    app.get('/pedalboards', (req, res) => {
+      return collection
+      .find()
+      .toArray()
+      .then(data => {
+        res.json(data)
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(500)
+      })
+    })
+
+    app.listen(3000, () => console.log('Listening on port 3000'))
+  })
