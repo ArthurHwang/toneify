fd7543802de7a63d802e75d290e2a154b6b5fbc7
ArthurHwang <ahwang55@gmail.com>
2018-07-23 11:04:47 -0700

    Make a .env file to setup MONGODB_URI #1

--- database/setup.js
+++ database/setup.js
@@ -2,7 +2,17 @@ require('dotenv/config');
 const { MongoClient } = require('mongodb');
 const seeddData = require('./seed');
 
-MongoClient.connect(
-  process.env.MONGODB_URI,
-  (useNewUrlParse: true)
-);
+const MONGODB_URI = process.env.MONGODB_URI
+
+MongoClient
+  .connect(MONGODB_URI, {useNewUrlParse: true})
+  .then(client => {
+    const db = client.db('toneify')
+    const collection = db.collection('pedalboards')
+
+    // const remove = collection.remove()
+    // const see = collection.insertMany(seedData)
+
+
+    return collection.removeMany()
+  })
