e35db79f6ebcec7dc66e4d8985d2e88d43de958a
ArthurHwang <ahwang55@gmail.com>
2018-07-24 10:45:31 -0700

    Ensure that card output is uniform #1

--- client/components/Pedalboards/Pedalboard/Pedalboard.js
+++ client/components/Pedalboards/Pedalboard/Pedalboard.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Button, Card, Image } from 'semantic-ui-react'
+import { Container, Button, Card, Image } from 'semantic-ui-react'
 
 const pedalBoard = ({ price, brand, model, description, image }) => {
   return (
     <Card>
+      <Image src={image} style={{ height: '150px' }} />
       <Card.Content>
-        <Image src={image} />
         <Card.Header>{brand}</Card.Header>
         <Card.Meta>{model}</Card.Meta>
         <Card.Description>
