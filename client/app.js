d62336744081f996f4f416129aa38f4bfa1c0b80
ArthurHwang <ahwang55@gmail.com>
2018-07-24 14:34:21 -0700

    Add a click listener for each rendered pedalboard #18

--- client/app.js
+++ client/app.js
@@ -1,13 +1,18 @@
 import React, { Component, Fragment } from 'react'
 import Nav from './components/Nav/Nav'
 import PedalBoards from './components/PedalBoards/PedalBoards'
+import Modal from './components/Modal/Modal'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      pedalboards: []
+      pedalboards: [],
+      currentPedalboard: null,
+      modalOpen: false
     }
+    this.handlePedalBoardClick = this.handlePedalBoardClick.bind(this)
+    this.handleModalClick = this.handleModalClick.bind(this)
   }
 
   componentDidMount() {
@@ -21,12 +26,37 @@ class App extends Component {
       .catch(err => console.log(err))
   }
 
+  handlePedalBoardClick(id, event) {
+    const foundPedalBoard = this.state.pedalboards.find((elem, index) => {
+      if (index === id) {
+        return elem
+      }
+    })
+
+    this.setState({ modalOpen: true, currentPedalboard: foundPedalBoard })
+  }
+
+  handleModalClick(event) {
+    this.setState({ modalOpen: false })
+  }
+
   render() {
-    const { pedalboards } = this.state
+    const { pedalboards, currentPedalboard } = this.state
+
+    const modal = this.state.modalOpen ? (
+      <Modal
+        handleClick={this.handleModalClick}
+        pedalData={currentPedalboard}
+      />
+    ) : null
     return (
       <Fragment>
+        {modal}
         <Nav />
-        <PedalBoards pedalData={pedalboards} />
+        <PedalBoards
+          handleClick={this.handlePedalBoardClick}
+          pedalData={pedalboards}
+        />
       </Fragment>
     )
   }
