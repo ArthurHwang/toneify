a7bcc7b1c261b66508766444e26db0975577d65f
ArthurHwang <ahwang55@gmail.com>
2018-07-25 13:29:27 -0700

    Define a pedalboard builder route with react router that renders the builder component #2

---
+++ client/containers/Builder/Builder.js
@@ -0,0 +1,9 @@
+import React, { Component, Fragment } from 'react'
+
+class Builder extends Component {
+  render() {
+    return <h1> Hello Friend </h1>
+  }
+}
+
+export default Builder
---
+++ client/containers/PedalboardsView/PedalboardsView.js
@@ -0,0 +1,62 @@
+import React, { Component, Fragment } from 'react'
+// import Nav from './components/Nav/Nav'
+import PedalBoards from '../../components/PedalBoards/PedalBoards'
+import Modal from '../../components/Modal/Modal'
+
+class Pedalboards extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pedalboards: [],
+      currentPedalboard: null,
+      modalOpen: false
+    }
+  }
+
+  componentDidMount() {
+    fetch('/pedalboards', {
+      method: 'GET'
+    })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ pedalboards: data })
+      })
+      .catch(err => console.log(err))
+  }
+
+  handlePedalBoardClick = (id, event) => {
+    const foundPedalBoard = this.state.pedalboards.find((elem, index) => {
+      if (elem.id === id) {
+        return elem
+      }
+    })
+    this.setState({ modalOpen: true, currentPedalboard: foundPedalBoard })
+  }
+
+  handleModalClick = event => {
+    this.setState({ modalOpen: false })
+  }
+
+  render() {
+    const { modalOpen, pedalboards, currentPedalboard } = this.state
+    const modal = this.state.modalOpen ? (
+      <Modal
+        handleClick={this.handleModalClick}
+        pedalData={currentPedalboard}
+        modalOpen={modalOpen}
+      />
+    ) : null
+    return (
+      <Fragment>
+        {modal}
+        {/* <Nav /> */}
+        <PedalBoards
+          handleClick={this.handlePedalBoardClick}
+          pedalData={pedalboards}
+        />
+      </Fragment>
+    )
+  }
+}
+
+export default Pedalboards
