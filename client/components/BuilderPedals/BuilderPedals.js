4d60cddddc8f31d03bd57ef7b144a4a5f644c72c
ArthurHwang <ahwang55@gmail.com>
2018-07-29 11:41:39 -0700

    Create a semantic-ui component scroll-able modal that will display list of pedals the user can add #6

--- client/components/BuilderPedals/BuilderPedals.js
+++ client/components/BuilderPedals/BuilderPedals.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import { Image } from 'semantic-ui-react'
 
-const builderPedals = ({ pedals }) => {
-  let pedalOutput = pedals.map((elem, index) => (
-    <Image
-      key={index}
-      style={{
-        height: '200px',
-        width: '135px',
-        position: 'relative',
-        display: 'inline-block'
-      }}
-      src={elem.image}
-    />
-  ))
-  return pedalOutput
-}
+const builderPedals = ({ pedals }) => (
+  <div style={{ position: 'absolute', top: '20%', left: '29%' }}>
+    {pedals.map((elem, index) => (
+      <Image
+        key={elem.model}
+        src={elem.image}
+        style={{
+          height: '200px',
+          width: '135px',
+          position: 'relative',
+          display: 'inline-block'
+        }}
+      />
+    ))}
+  </div>
+)
 
 export default builderPedals
