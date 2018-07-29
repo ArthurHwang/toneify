1cc7a091db7920d8a226f229bfef8919279f34c2
ArthurHwang <ahwang55@gmail.com>
2018-07-29 11:42:06 -0700

    When the user clicks on BuilderAddPedalButton it should pop-out the modal #6

--- client/components/Modal/BuilderModal/BuilderModal.js
+++ client/components/Modal/BuilderModal/BuilderModal.js
@@ -1,40 +1,22 @@
 import React from 'react'
-import { Button, Icon, Modal } from 'semantic-ui-react'
+import { Modal } from 'semantic-ui-react'
 import Pedals from '../../Pedals/Pedals'
 
-const builderModal = ({ handleClick, pedalData }) => (
-  <Modal
-    open
-    dimmer="blurring"
-    style={{ top: '13%', width: '1200px', left: '40%' }}
-    trigger={(
-      <Button
-        color="black"
-        style={{
-          marginTop: '10px',
-          marginRight: '0',
-          float: 'right',
-          height: '48px',
-          width: '150px',
-          display: 'inline'
-        }}
-      >
-        <Icon size="large" color="green" name="add" />
-        <span
-          style={{
-            color: '#21ba45'
-          }}
-        >
-          Add Pedal
-        </span>
-      </Button>
-    )}
-  >
-    <Modal.Header>Click a Pedal to Add to Pedalboard</Modal.Header>
-    <Modal.Content image scrolling>
-      <Pedals handleClick={handleClick} pedalData={pedalData} />
-    </Modal.Content>
-  </Modal>
-)
+const builderModal = ({ closeModalHandler, showModal, handleClick, pedalData }) => {
+  let modalOutput = null
+
+  if (showModal) {
+    modalOutput = (
+      <Modal onClick={closeModalHandler} open dimmer="blurring" style={{ position: 'absolute', top: '13%' }}>
+        <Modal.Header>Click a Pedal to Add to Pedalboard</Modal.Header>
+        <Modal.Content image scrolling>
+          <Pedals handleClick={handleClick} pedalData={pedalData} />
+        </Modal.Content>
+      </Modal>
+    )
+  }
+
+  return modalOutput
+}
 
 export default builderModal
