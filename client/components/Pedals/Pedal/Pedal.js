434188c8e83f5ccf1a548c4393e3fde319c5d025
ArthurHwang <ahwang55@gmail.com>
2018-07-29 11:43:05 -0700

    Define a method pedalAddHandler that finds the correct pedal and updates state with the current user pedals #6

--- client/components/Pedals/Pedal/Pedal.js
+++ client/components/Pedals/Pedal/Pedal.js
@@ -1,46 +1,35 @@
 import React from 'react'
 import { Icon, Card, Image } from 'semantic-ui-react'
 
-const pedal = ({
-  id,
-  price,
-  brand,
-  model,
-  description,
-  image,
-  type,
-  handleClick
-}) => {
-  return (
-    <Card raised onClick={e => handleClick(id, e)}>
-      <Image
-        src={image}
-        style={{
-          height: '200px',
-          width: '135px',
-          backgroundSize: 'contain',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          margin: '15px 15px 15px 25%'
-        }}
-      />
-      <Card.Content extra>
-        <Card.Header>{brand}</Card.Header>
-        <Card.Meta>
-          <span style={{ color: '#bc003f' }}>{model}</span>
-        </Card.Meta>
-        <Card.Meta>
-          <span style={{ color: '#bc003f' }}>{type}</span>
-        </Card.Meta>
-        <Card.Description>
-          <strong>
-            <Icon name="money bill alternate" />
-            {price}
-          </strong>
-        </Card.Description>
-      </Card.Content>
-    </Card>
-  )
-}
+const pedal = ({ id, price, brand, model, description, image, type, handleClick }) => (
+  <Card raised onClick={e => handleClick(id, e)}>
+    <Image
+      src={image}
+      style={{
+        height: '200px',
+        width: '135px',
+        backgroundSize: 'contain',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        margin: '10px auto'
+      }}
+    />
+    <Card.Content extra>
+      <Card.Header>{brand}</Card.Header>
+      <Card.Meta>
+        <span style={{ color: '#bc003f' }}>{model}</span>
+      </Card.Meta>
+      <Card.Meta>
+        <span style={{ color: '#bc003f' }}>{type}</span>
+      </Card.Meta>
+      <Card.Description>
+        <strong>
+          <Icon name="money bill alternate" />
+          {price}
+        </strong>
+      </Card.Description>
+    </Card.Content>
+  </Card>
+)
 
 export default pedal
