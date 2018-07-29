cf56178233efa2520c5ad80576e5cb0be1f056d8
ArthurHwang <ahwang55@gmail.com>
2018-07-29 15:31:05 -0700

    User should be able to position pedals freely #11

--- client/components/BuilderPedals/BuilderPedals.js
+++ client/components/BuilderPedals/BuilderPedals.js
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Image } from 'semantic-ui-react'
+import Draggable from 'react-draggable'
 
-const builderPedals = ({ pedals }) => (
-  <div style={{ position: 'absolute', top: '20%', left: '29%' }}>
-    {pedals.map((elem, index) => (
-      <Image
-        key={elem.model}
-        src={elem.image}
-        style={{
-          height: '200px',
-          width: '135px',
-          position: 'relative',
-          display: 'inline-block'
-        }}
-      />
-    ))}
-  </div>
-)
+class builderPedals extends Component {
+  render() {
+    return (
+      <div style={{ position: 'absolute', top: '20%', left: '29%' }}>
+        {this.props.pedals.map((elem, index) => (
+          <Draggable key={elem.id}>
+            <Image
+              id={elem.id}
+              item={elem}
+              src={elem.image}
+              style={{
+                height: '200px',
+                width: '135px',
+                position: 'relative',
+                display: 'inline-block'
+              }}
+            />
+          </Draggable>
+        ))}
+      </div>
+    )
+  }
+}
 
 export default builderPedals
--- client/components/PedalboardBuilderDisplay/PedalboardBuilderDisplay.js
+++ client/components/PedalboardBuilderDisplay/PedalboardBuilderDisplay.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Image } from 'semantic-ui-react'
+// import { DropTarget } from 'react-dnd'
+//
+// function collect(connect, monitor) {
+//   console.log('what')
+//   return {
+//     connectDropTarget: connect.dropTarget(),
+//     hovered: monitor.isOver(),
+//     item: monitor.getItem()
+//   }
+// }
 
-const PedalboardBuilderDisplay = ({ pedals, currentPedalboard }) => (
-  <Image size="massive" src={currentPedalboard.topView} centered />
-)
+class PedalboardBuilderDisplay extends Component {
+  render() {
+    // const { connectDropTarget, hovered, item } = this.props
+    return (
+      <div>
+        <Image size="massive" src={this.props.currentPedalboard.topView} centered />
+      </div>
+    )
+  }
+}
 
+// }= ({ pedals, currentPedalboard }) => (
+//
+// )
+
+// export default DropTarget('item', {}, collect)(PedalboardBuilderDisplay)
 export default PedalboardBuilderDisplay
