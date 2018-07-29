e43ad5c86d588705a64cb9ae93f6fae39297983a
ArthurHwang <ahwang55@gmail.com>
2018-07-29 11:46:04 -0700

    Ensure that when the user adds a pedal to the build, it can be rendered on top of the pedalboard #6

--- client/components/PedalboardBuilderDisplay/PedalboardBuilderDisplay.js
+++ client/components/PedalboardBuilderDisplay/PedalboardBuilderDisplay.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Image } from 'semantic-ui-react'
 
-const PedalboardBuilderDisplay = ({ currentPedalboard }) => (
+const PedalboardBuilderDisplay = ({ pedals, currentPedalboard }) => (
   <Image size="massive" src={currentPedalboard.topView} centered />
 )
 
