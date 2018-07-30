af4bf8269d2f1d9443a6dae668eaae289857bd79
ArthurHwang <ahwang55@gmail.com>
2018-07-29 21:40:32 -0700

    Add onMouseEnterlistener to pedals to conditionally show the rotate button for each specific pedal #15

--- client/components/Pedals/Pedals.js
+++ client/components/Pedals/Pedals.js
@@ -1,29 +1,25 @@
 import React from 'react'
-import Pedal from './Pedal/Pedal'
 import { Container, Card } from 'semantic-ui-react'
+import Pedal from './Pedal/Pedal'
 
-const pedals = ({ pedalData, handleClick }) => {
-  return (
-    <Container>
-      <Card.Group itemsPerRow={4} className="ui center aligned grid">
-        {pedalData.map((elem, index) => {
-          return (
-            <Pedal
-              handleClick={handleClick}
-              key={index}
-              id={elem.id}
-              image={elem.image}
-              brand={elem.brand}
-              description={elem.description}
-              model={elem.model}
-              price={elem.price}
-              type={elem.type}
-            />
-          )
-        })}
-      </Card.Group>
-    </Container>
-  )
-}
+const pedals = ({ pedalData, handleClick }) => (
+  <Container>
+    <Card.Group itemsPerRow={4} className="ui center aligned grid">
+      {pedalData.map((elem, index) => (
+        <Pedal
+          handleClick={handleClick}
+          key={elem.model}
+          id={elem.id}
+          image={elem.image}
+          brand={elem.brand}
+          description={elem.description}
+          model={elem.model}
+          price={elem.price}
+          type={elem.type}
+        />
+      ))}
+    </Card.Group>
+  </Container>
+)
 
 export default pedals
