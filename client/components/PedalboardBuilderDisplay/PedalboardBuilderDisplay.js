cb0d980495787335e4bfa2d58fabe54eb5d482ef
ArthurHwang <ahwang55@gmail.com>
2018-07-30 13:40:10 -0700

    Switch PedalboardBuilderDisplay to function component #11

--- client/components/PedalboardBuilderDisplay/PedalboardBuilderDisplay.js
+++ client/components/PedalboardBuilderDisplay/PedalboardBuilderDisplay.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
 import { Image } from 'semantic-ui-react'
 
-class PedalboardBuilderDisplay extends Component {
-  render() {
-    return (
-      <div>
-        <Image size="massive" src={this.props.currentPedalboard.topView} centered />
-      </div>
-    )
-  }
+const PedalboardBuilderDisplay = ({ currentPedalboard }) => {
+  return (
+    <div>
+      <Image size="massive" src={currentPedalboard.topView} centered />
+    </div>
+  )
 }
 
 export default PedalboardBuilderDisplay
