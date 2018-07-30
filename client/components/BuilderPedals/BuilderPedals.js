dfc5c9c1d8659094bbba26d90d3af0d02b37189d
ArthurHwang <ahwang55@gmail.com>
2018-07-30 13:39:45 -0700

    Switch BuilderPedals.js to function component #11

--- client/components/BuilderPedals/BuilderPedals.js
+++ client/components/BuilderPedals/BuilderPedals.js
@@ -2,28 +2,26 @@ import React, { Component } from 'react'
 import { Image } from 'semantic-ui-react'
 import Draggable from 'react-draggable'
 
-class builderPedals extends Component {
-  render() {
-    return (
-      <div style={{ position: 'absolute', top: '20%', left: '29%' }}>
-        {this.props.pedals.map((elem, index) => (
-          <Draggable key={elem.id}>
-            <Image
-              id={elem.id}
-              item={elem}
-              src={elem.image}
-              style={{
-                height: '200px',
-                width: '135px',
-                position: 'relative',
-                display: 'inline-block'
-              }}
-            />
-          </Draggable>
-        ))}
-      </div>
-    )
-  }
+const BuilderPedals = ({ pedals }) => {
+  return (
+    <div style={{ position: 'absolute', top: '20%', left: '29%' }}>
+      {pedals.map((elem, index) => (
+        <Draggable key={elem.id}>
+          <Image
+            id={elem.id}
+            item={elem}
+            src={elem.image}
+            style={{
+              height: '200px',
+              width: '135px',
+              position: 'relative',
+              display: 'inline-block'
+            }}
+          />
+        </Draggable>
+      ))}
+    </div>
+  )
 }
 
-export default builderPedals
+export default BuilderPedals
