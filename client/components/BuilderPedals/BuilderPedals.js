5a03f1d47170c9ec1363d1a0822579b775d201c8
ArthurHwang <ahwang55@gmail.com>
2018-07-29 21:39:21 -0700

    Enhance pedalAddHandlermethod to copy and mutate the original array of pedalsOnBoard to include new properties showRotateButton and Rotate #15

--- client/components/BuilderPedals/BuilderPedals.js
+++ client/components/BuilderPedals/BuilderPedals.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react'
-import { Image } from 'semantic-ui-react'
+import { Image, Icon } from 'semantic-ui-react'
 import Draggable from 'react-draggable'
 
 class builderPedals extends Component {
   render() {
     return (
-      <div style={{ position: 'absolute', top: '20%', left: '29%' }}>
+      <div style={{ display: 'flex', flexDirection: 'row', position: 'absolute', top: '20%', left: '29%' }}>
         {this.props.pedals.map((elem, index) => (
           <Draggable key={elem.id}>
-            <Image
-              id={elem.id}
-              item={elem}
-              src={elem.image}
-              style={{
-                height: '200px',
-                width: '135px',
-                position: 'relative',
-                display: 'inline-block'
-              }}
-            />
+            <div>
+              {elem.showRotate ? (
+                <Icon
+                  onMouseEnter={event => this.props.mouseOver(elem.id, event)}
+                  onMouseLeave={event => this.props.mouseLeave(elem.id, event)}
+                  style={{
+                    fontSize: '40px',
+                    zIndex: '2000',
+                    color: '#bc003f',
+                    position: 'absolute',
+                    top: '-5px',
+                    left: '32%'
+                  }}
+                  onClick={event => this.props.rotate(elem.id, event)}
+                  name="undo"
+                />
+              ) : null}
+
+              <Image
+                onMouseEnter={event => this.props.mouseOver(elem.id, event)}
+                onMouseLeave={event => this.props.mouseLeave(elem.id, event)}
+                id={elem.id}
+                item={elem}
+                src={elem.image}
+                style={{
+                  transform: `rotate(${elem.rotation}deg)`,
+                  height: '200px',
+                  width: '135px',
+                  position: 'relative',
+                  display: 'inline-block'
+                }}
+              />
+            </div>
           </Draggable>
         ))}
       </div>
