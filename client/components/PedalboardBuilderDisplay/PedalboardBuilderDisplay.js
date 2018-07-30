823b14369904870ed01b9bca2d7d0732b2358711
ArthurHwang <ahwang55@gmail.com>
2018-07-29 21:43:20 -0700

    Ensure that each pedals' rotate button should render only for itself #15

--- client/components/PedalboardBuilderDisplay/PedalboardBuilderDisplay.js
+++ client/components/PedalboardBuilderDisplay/PedalboardBuilderDisplay.js
@@ -1,19 +1,8 @@
 import React, { Component } from 'react'
 import { Image } from 'semantic-ui-react'
-// import { DropTarget } from 'react-dnd'
-//
-// function collect(connect, monitor) {
-//   console.log('what')
-//   return {
-//     connectDropTarget: connect.dropTarget(),
-//     hovered: monitor.isOver(),
-//     item: monitor.getItem()
-//   }
-// }
 
 class PedalboardBuilderDisplay extends Component {
   render() {
-    // const { connectDropTarget, hovered, item } = this.props
     return (
       <div>
         <Image size="massive" src={this.props.currentPedalboard.topView} centered />
@@ -22,9 +11,4 @@ class PedalboardBuilderDisplay extends Component {
   }
 }
 
-// }= ({ pedals, currentPedalboard }) => (
-//
-// )
-
-// export default DropTarget('item', {}, collect)(PedalboardBuilderDisplay)
 export default PedalboardBuilderDisplay
