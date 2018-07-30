ec3bbdee57262e34502a3d5ced0713f09f8aa20c
ArthurHwang <ahwang55@gmail.com>
2018-07-29 21:41:40 -0700

    Add onMouseEnter listener to rotate button to protect element from buggy rendering #15

--- client/components/Pedalboards/Pedalboards.js
+++ client/components/Pedalboards/Pedalboards.js
@@ -1,28 +1,24 @@
 import React from 'react'
-import PedalBoard from './Pedalboard/Pedalboard'
 import { Container, Card } from 'semantic-ui-react'
+import PedalBoard from './Pedalboard/Pedalboard'
 
-const pedalBoards = ({ handleClick, pedalData }) => {
-  return (
-    <Container>
-      <Card.Group itemsPerRow={4} className="ui center aligned grid">
-        {pedalData.map((elem, index) => {
-          return (
-            <PedalBoard
-              key={index}
-              id={elem.id}
-              handleClick={handleClick}
-              image={elem.image}
-              brand={elem.brand}
-              description={elem.description}
-              model={elem.model}
-              price={elem.price}
-            />
-          )
-        })}
-      </Card.Group>
-    </Container>
-  )
-}
+const pedalBoards = ({ handleClick, pedalData }) => (
+  <Container>
+    <Card.Group itemsPerRow={4} className="ui center aligned grid">
+      {pedalData.map((elem, index) => (
+        <PedalBoard
+          key={elem.model}
+          id={elem.id}
+          handleClick={handleClick}
+          image={elem.image}
+          brand={elem.brand}
+          description={elem.description}
+          model={elem.model}
+          price={elem.price}
+        />
+      ))}
+    </Card.Group>
+  </Container>
+)
 
 export default pedalBoards
