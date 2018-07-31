c3edc122b05e67897bc88991b3de3876d130ed3e
ArthurHwang <ahwang55@gmail.com>
2018-07-31 11:30:23 -0700

    Move non-dynamic styles outside of components to styles.css #10

--- client/components/Builder/BuilderPedals.js
+++ client/components/Builder/BuilderPedals.js
@@ -2,68 +2,44 @@ import React from 'react'
 import { Image, Icon } from 'semantic-ui-react'
 import Draggable from 'react-draggable'
 
-const BuilderPedals = ({ mouseLeave, mouseOver, rotate, pedals, deletePedal }) => {
-  const styles = {
-    pedals: {
-      display: 'flex',
-      flexDirection: 'row',
-      position: 'absolute',
-      top: '20%',
-      left: '29%'
-    },
-    iconRotate: {
-      fontSize: '35px',
-      zIndex: '2000',
-      color: '#bc003f',
-      position: 'absolute',
-      top: '-5px',
-      left: '34%'
-    },
-    iconDelete: {
-      fontSize: '40px',
-      zIndex: '2000',
-      color: '#bc003f',
-      position: 'absolute',
-      bottom: '-25px',
-      left: '32%'
-    },
-    image: {
-      height: '200px',
-      width: '135px',
-      position: 'relative',
-      display: 'inline-block'
-    }
-  }
-
+const BuilderPedals = ({
+  mouseLeave,
+  mouseOver,
+  rotate,
+  pedals,
+  deletePedal
+}) => {
   return (
-    <div style={styles.pedals}>
+    <div className="pedal-on-board-container">
       {pedals.map((elem, index) => (
         <Draggable key={elem.id}>
           <div>
             {elem.showButtons && (
               <Icon
+                size="large"
+                className="icon-rotate"
                 onMouseEnter={event => mouseOver(elem.id, event)}
                 onMouseLeave={event => mouseLeave(elem.id, event)}
-                style={styles.iconRotate}
                 onClick={event => rotate(elem.id, event)}
                 name="refresh"
               />
             )}
             <Image
+              className="pedal-on-board"
               onMouseEnter={event => mouseOver(elem.id, event)}
               onMouseLeave={event => mouseLeave(elem.id, event)}
               item={elem}
               src={elem.image}
               style={{
-                ...styles.image,
                 transform: `rotate(${elem.rotation}deg)`
               }}
             />
             {elem.showButtons && (
               <Icon
+                size="big"
+                className="icon-delete"
                 onMouseEnter={event => mouseOver(elem.id, event)}
                 onMouseLeave={event => mouseLeave(elem.id, event)}
-                style={styles.iconDelete}
                 onClick={event => deletePedal(elem.id, event)}
                 name="delete"
               />
