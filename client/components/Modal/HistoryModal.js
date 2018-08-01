b668ad2325942e4e8177eff721e478208a8a20ae
ArthurHwang <ahwang55@gmail.com>
2018-08-01 10:45:20 -0700

    Create a history button that will show the users previously saved builds #13

--- client/components/Modal/HistoryModal.js
+++ client/components/Modal/HistoryModal.js
@@ -3,25 +3,33 @@ import { Icon, Table, Modal } from 'semantic-ui-react'
 
 const HistoryModal = ({ closeModalHandler, showModal, buildHistory }) => {
   const modalOutput = showModal && (
-    <Modal onClick={closeModalHandler} open dimmer="blurring" style={{ position: 'absolute', top: '13%' }}>
+    <Modal
+      onClick={closeModalHandler}
+      open
+      dimmer="blurring"
+      style={{ position: 'absolute', top: '13%' }}>
       <Modal.Header>Saved Builds</Modal.Header>
       <Modal.Content>
         <Table striped>
           <Table.Header>
             <Table.Row>
-              <Table.HeaderCell>Time</Table.HeaderCell>
-              <Table.HeaderCell>PedalBoard</Table.HeaderCell>
-              <Table.HeaderCell>Pedals</Table.HeaderCell>
-              <Table.HeaderCell>Exchange Build</Table.HeaderCell>
+              <Table.HeaderCell textAlign="center">Time</Table.HeaderCell>
+              <Table.HeaderCell textAlign="center">PedalBoard</Table.HeaderCell>
+              <Table.HeaderCell textAlign="center">Pedals</Table.HeaderCell>
+              <Table.HeaderCell textAlign="center">
+                Exchange Build
+              </Table.HeaderCell>
             </Table.Row>
           </Table.Header>
 
           <Table.Body>
             {buildHistory.map(elem => (
-              <Table.Row key={elem.id} verticalAlign="top">
+              <Table.Row key={elem.id} textAlign="center" verticalAlign="top">
                 <Table.Cell>{elem.timeStamp}</Table.Cell>
-                <Table.Cell>{`${elem.pedalBoard.brand} ${elem.pedalBoard.model}`}</Table.Cell>
-                <Table.Cell verticalAlign="top">
+                <Table.Cell>{`${elem.pedalBoard.brand} ${
+                  elem.pedalBoard.model
+                }`}</Table.Cell>
+                <Table.Cell textAlign="center" verticalAlign="top">
                   {elem.pedals.map(pedal => (
                     <strong key={pedal.id}>
                       {`${pedal.brand} ${pedal.model}`}
@@ -29,8 +37,8 @@ const HistoryModal = ({ closeModalHandler, showModal, buildHistory }) => {
                     </strong>
                   ))}
                 </Table.Cell>
-                <Table.Cell verticalAlign="top">
-                  <Icon name="exchange" />
+                <Table.Cell textAlign="center" verticalAlign="top">
+                  <Icon color="orange" name="exchange" />
                 </Table.Cell>
               </Table.Row>
             ))}
