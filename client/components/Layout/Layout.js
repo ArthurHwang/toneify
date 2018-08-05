1df2ddaf76fd3a271971985f1dacf9ee2afa9d2a
ArthurHwang <ahwang55@gmail.com>
2018-08-05 02:07:55 -0700

    Add withRouter HOC to Layout.js to conditionally render navbar based on triggered route #39

--- client/components/Layout/Layout.js
+++ client/components/Layout/Layout.js
@@ -1,15 +1,31 @@
 import React, { Component, Fragment } from 'react'
+import { withRouter } from 'react-router-dom'
 import Nav from '../Nav/Nav'
 
 class Layout extends Component {
+  state = {
+    hash: null
+  }
+
+  componentWillMount() {
+    this.unlisten = this.props.history.listen((location, action) => {
+      this.setState({ hash: location.pathname })
+    })
+  }
+
+  componentWillUnmount() {
+    this.unlisten()
+  }
+
   render() {
+    const navOutput = this.state.hash === '/' ? null : <Nav />
     return (
       <Fragment>
-        <Nav />
+        {navOutput}
         <main>{this.props.children}</main>
       </Fragment>
     )
   }
 }
 
-export default Layout
+export default withRouter(Layout)
