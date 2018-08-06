abccda67af1dc6230b18b46feb08d406f5148e5f
ArthurHwang <ahwang55@gmail.com>
2018-08-05 22:06:00 -0700

    Enhance PedalsModal.js to output a semantic ui table component for users to view tech specs #83

--- client/components/Modal/PedalsModal.js
+++ client/components/Modal/PedalsModal.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Icon, Button, Header, Image, Modal } from 'semantic-ui-react'
+import { Icon, Button, Header, Image, Modal, Divider, Table } from 'semantic-ui-react'
 
 const PedalsModal = ({ handleBuildClick, handleClick, pedalData, modalOpen }) => {
   let ModalOutput = modalOpen && (
-    <Modal style={{ top: '38%' }} open size="large" onClick={handleClick} closeIcon>
+    <Modal style={{ top: '10%' }} open size="large" onClick={handleClick} closeIcon>
       <Modal.Header>
         {pedalData.brand}
         <Button onClick={handleBuildClick} compact floated="right" primary>
           Add to Pedalboard
         </Button>
       </Modal.Header>
-      <Modal.Content image>
+      <Modal.Content scrolling image>
         <Image
           className="pedal-modal-image"
           wrapped
@@ -28,17 +28,88 @@ const PedalsModal = ({ handleBuildClick, handleClick, pedalData, modalOpen }) =>
             </div>
             <div>
               <strong>
-                <Icon name="money bill alternate" />
+                <Icon color="green" name="money bill alternate" />
                 {pedalData.price}
               </strong>
             </div>
           </Header>
-          <div>{pedalData.description}</div>
+          <p style={{ fontSize: '1.2rem' }}>{pedalData.description}</p>
         </Modal.Description>
+      </Modal.Content>
+      <Divider />
+      <Modal.Content style={{ paddingTop: '0' }}>
+        <Table compact striped>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell colSpan="2">Technical Specifications</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            <Table.Row verticalAlign="middle">
+              <Table.Cell width={3}>
+                <strong>Type</strong>
+              </Table.Cell>
+              <Table.Cell>{pedalData.specs.type}</Table.Cell>
+            </Table.Row>
+            <Table.Row verticalAlign="middle">
+              <Table.Cell>
+                <strong>Inputs</strong>
+              </Table.Cell>
+              <Table.Cell>{pedalData.specs.inputs}</Table.Cell>
+            </Table.Row>
+            <Table.Row verticalAlign="middle">
+              <Table.Cell>
+                <strong>Outputs</strong>
+              </Table.Cell>
+              <Table.Cell>{pedalData.specs.outputs}</Table.Cell>
+            </Table.Row>
+            <Table.Row verticalAlign="middle">
+              <Table.Cell>
+                <strong>Batteries</strong>
+              </Table.Cell>
+              <Table.Cell>{pedalData.specs.batteries}</Table.Cell>
+            </Table.Row>
+            <Table.Row verticalAlign="middle">
+              <Table.Cell>
+                <strong>Height</strong>
+              </Table.Cell>
+              <Table.Cell>{pedalData.specs.height}</Table.Cell>
+            </Table.Row>
+            <Table.Row verticalAlign="middle">
+              <Table.Cell>
+                <strong>Width</strong>
+              </Table.Cell>
+              <Table.Cell>{pedalData.specs.width}</Table.Cell>
+            </Table.Row>
+            <Table.Row verticalAlign="middle">
+              <Table.Cell>
+                <strong>Depth</strong>
+              </Table.Cell>
+              <Table.Cell>{pedalData.specs.depth}</Table.Cell>
+            </Table.Row>
+            <Table.Row verticalAlign="middle">
+              <Table.Cell textAlign="left">
+                <strong>Weight</strong>
+              </Table.Cell>
+              <Table.Cell>{pedalData.specs.weight}</Table.Cell>
+            </Table.Row>
+            <Table.Row verticalAlign="middle">
+              <Table.Cell>
+                <strong>Power Supply</strong>
+              </Table.Cell>
+              <Table.Cell>{pedalData.specs.pwrSupply}</Table.Cell>
+            </Table.Row>
+            <Table.Row verticalAlign="middle">
+              <Table.Cell>
+                <strong>SKU</strong>
+              </Table.Cell>
+              <Table.Cell>{pedalData.specs.sku}</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
       </Modal.Content>
     </Modal>
   )
-
   return ModalOutput
 }
 
