231bc3d3c2849be923b84b969772cca35e99a365
ArthurHwang <ahwang55@gmail.com>
2018-08-08 19:43:40 -0700

    Ensure that if there has been no doubleClicked pedal, there will be no rendered output #66

--- client/components/Builder/BuilderTitle.js
+++ client/components/Builder/BuilderTitle.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { Message } from 'semantic-ui-react'
 
 const BuilderTitle = ({ pedalboardName }) => (
-  <Message color="black" compact>
-    <span style={{ fontSize: '1.2rem', color: '#bc003f' }}>Pedalboard: </span>
+  <Message style={{position:'absolute'}} color="black" compact>
+    <span style={{fontSize: '1.2rem', color: '#bc003f' }}>Pedalboard: </span>
     <strong>
       {pedalboardName.brand} {pedalboardName.model}
     </strong>
--- client/components/Builder/YoutubePedalsOutput.js
+++ client/components/Builder/YoutubePedalsOutput.js
@@ -4,19 +4,23 @@ import YoutubeFrame from 'react-youtube'
 
 const YoutubePedalsOutput = ({ searchResults }) => {
   const opts = {
-    width: "300",
-    height: "180"
+    width: '300',
+    height: '200'
+  }
+  const styles = {
+    container: {
+      backgroundColor: '#1b1c1d',
+      marginTop: '15px',
+      padding: '20px 0'
+    }
   }
   return (
-    <Container textAlign='center'>
-      {searchResults.map(video => (
-        <YoutubeFrame key={video.id} opts={opts} videoId={video.id} />
-      ))}
-    </Container>
+    searchResults.length > 0 && (
+      <Container fluid style={styles.container} textAlign="center">
+          {searchResults.map(video => <YoutubeFrame  key={video.id} opts={opts} videoId={video.id} />)}
+      </Container>
+    )
   )
 }
-
-
-
 
 export default YoutubePedalsOutput
