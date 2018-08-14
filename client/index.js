a3e8e0fc4beb3c8c58bc8e9433c37a9b0b877966
ArthurHwang <ahwang55@gmail.com>
2018-08-13 18:11:03 -0700

    Link project to reduxand redux-thunk by adding needed packages in index.js to hook up project #93

--- client/index.js
+++ client/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+import { builderReducer } from './store/reducers/builder'
 import App from './app'
 
 ReactDOM.render(
