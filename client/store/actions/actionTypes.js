a84e20d5d88ac4a01636d304db8305df385a8f06
ArthurHwang <ahwang55@gmail.com>
2018-08-13 20:25:10 -0700

    Create actionTypes.js file #93

---
+++ client/store/actions/actionTypes.js
@@ -0,0 +1,6 @@
+export const ADD_PEDAL = 'ADD_PEDAL'
+export const REMOVE_PEDAL = 'REMOVE_PEDAL'
+export const PEDALS_INIT = 'PEDALS_INIT'
+export const BUILD_HISTORY_INIT = 'BUILD_HISTORY_INIT'
+export const SET_BUILD_HISTORY = 'SET_BUILD_HISTORY'
+export const SET_PEDALS = 'SET_PEDALS'
