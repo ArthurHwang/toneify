b2d0205e53841faa2e2f63975b95d776fc1198c6
ArthurHwang <ahwang55@gmail.com>
2018-08-27 12:54:09 -0700

    Connect stripe component to redux store #95

--- client/components/Cart/StripeBilling.js
+++ client/components/Cart/StripeBilling.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react'
 import StripeCheckout from 'react-stripe-checkout'
-import { Container, Message } from 'semantic-ui-react'
+import { Container, Button } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import * as actions from '../../store/actions/index'
 
 class Payments extends Component {
   render() {
     return (
-      <Container>
+      <Container textAlign="center">
         <StripeCheckout
-          style={{ float: 'right', marginTop: '5px' }}
+          name="toneify"
+          description="Custom Pedalboard Configuration"
           amount={this.props.amount * 100}
-          token={token => console.log(token)}
+          token={token => this.props.handleToken(token)}
           stripeKey="pk_test_DnbMQwRiJo2rAXTS6QxmbgmA"
-        />
+        >
+          <Button primary>Checkout</Button>
+        </StripeCheckout>
       </Container>
     )
   }
 }
 
-export default Payments
+const mapDispatchToProps = dispatch => ({
+  handleToken: token => dispatch(actions.handleToken(token))
+})
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Payments)
